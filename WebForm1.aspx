59d690d5a5030371e8eca7d474eaf9bd0c73b9bb
PratikshaGhanawat <94125522+PratikshaGhanawat@users.noreply.github.com>
2023-12-22 20:50:23 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Question_5.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <label for="txtUserName">User Name:</label>
+            <asp:TextBox ID="txtUserName" runat="server"></asp:TextBox>
+            <br />
+            <label for="txtEmail">Email:</label>
+            <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnAddUser" Text="Add User" runat="server" OnClick="btnAddUser_Click" />
+            <br />
+            <span id="lblMessage" runat="server"></span>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Question_5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAddUser_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=abc;Integrated Security=True;";
+
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("InsertUser", connection))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    // Add parameters
+                    cmd.Parameters.AddWithValue("@UserName", txtUserName.Text);
+                    cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+
+                    try
+                    {
+                        connection.Open();
+                        cmd.ExecuteNonQuery();
+                        lblMessage.InnerText = "User added successfully!";
+                        ClearForm();
+                    }
+                    catch (Exception ex)
+                    {
+                        lblMessage.InnerText = "Error: " + ex.Message;
+                    }
+                }
+            }
+        }
+
+        private void ClearForm()
+        {
+            txtUserName.Text = "";
+            txtEmail.Text = "";
+        }
+    }
+}
